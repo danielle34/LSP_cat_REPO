e161726e36aa2689c02ca55716210152b7d82de8
danielle34 <danie3m3@gmail.com>
2024-09-11 21:16:06 -0400

    updated paths and added number checking

--- org/howard/edu/lsp/assignment2/WordCounting.java
+++ org/howard/edu/lsp/assignment2/WordCounting.java
@@ -3,46 +3,58 @@
 */
 package org.howard.edu.lsp.assignment2;
 
-import java.io.File; // Import the File class
-import java.io.FileNotFoundException; // Import this class to handle errors
-import java.util.ArrayList; // Import the Scanner class to read text files
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCounting {
 
+    // this method is from https://stackoverflow.com/questions/1102891/how-to-check-if-a-string-is-numeric-in-java
+    public static boolean isNumeric(String str) { 
+        try {  
+          Double.parseDouble(str);  
+          return true;
+        } catch(NumberFormatException e){  
+          return false;  
+        }  
+      }
+
     public static void main(String[] args) {
-        ArrayList<String> words = new ArrayList<>();
-        ArrayList<Integer> counts = new ArrayList<>();
+
         // https://www.w3schools.com/java/java_files_read.asp
         try {
+            ArrayList<String> words_list = new ArrayList<>();
+            ArrayList<Integer> count_times = new ArrayList<>();
 
-            // ArrayList<String> words = new ArrayList<>();
-            // ArrayList<Integer> counts = new ArrayList<>();
-
-            File myObj = new File("org/howard/edu/lsp/assignment2/words.text");
+            File myObj = new File("org/howard/edu/lsp/assignment2/words.txt");
             
             Scanner myReader = new Scanner(myObj);
-            System.out.println("oo"+myReader);
             while (myReader.hasNextLine()) {
                 // String data = myReader.nextLine();
                 // System.out.println(myReader.hasNext());
                 String word = myReader.next();
-                System.out.println(word);
+                if (isNumeric(word)==false && word.length()>3) 
+                {
+                    word = word.toLowerCase();
 
-                if (words.contains(word)){
-                    System.out.println("HEYY");
-                }
-                else{
-                    System.out.println("NO");
-                    words.add(word);
+                    if (words_list.contains(word)){
+                        count_times.set(words_list.indexOf(word), count_times.get(words_list.indexOf(word))+1);
+                    }
+                    else{
+                        words_list.add(word);
+                        count_times.add(1);
+                    }
                 }
 
-              
             }
             myReader.close();
+
+            for (int i = 0; i < words_list.size(); i++) {
+                System.out.println(words_list.get(i) +" "+ count_times.get(i));
+            }
         } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.println("There is an error.");
         }
     }
 }
