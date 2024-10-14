29fb54e961f30a7b98f7a8e629a45ba81c40d982
danielle34 <danie3m3@gmail.com>
2024-10-14 16:26:16 -0400

    made the hello world for questions 1-4

---
+++ org/howard/edu/lsp/midterm/question1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+*  Name: Morayo Adeyemi
+*/
+package org.howard.edu.lsp.assignment1.midterm.question1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
